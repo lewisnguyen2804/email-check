17b1142961a9fd55b9385ca87c748ceef75dab6e
Lewis Nguyen <lewis.nguyen@terralogic.com>
2025-04-24 16:32:04 +0700

    Update

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "HrosDev",
+      exec_mode: "cluster",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      env_local: {
+        NEXT_PUBLIC_API_KEY: "4CB8B3EE874A94E9F8EDEC369A9E4",
+      },
+    },
+  ],
+};
