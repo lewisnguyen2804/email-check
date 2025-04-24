1d2d0feac276c1197aaccad5832c7bac48caa96d
Lewis Nguyen <lewis.nguyen@terralogic.com>
2025-04-24 16:38:14 +0700

    update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,11 +2,11 @@ module.exports = {
   apps: [
     {
       name: "HrosDev",
-      exec_mode: "cluster",
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
+      exec_mode: "fork",
+      script: "node_modules/vite/bin/vite.js",
+      args: "preview",
       env_local: {
-        NEXT_PUBLIC_API_KEY: "4CB8B3EE874A94E9F8EDEC369A9E4",
+        VITE_API_KEY: "4CB8B3EE874A94E9F8EDEC369A9E4",
       },
     },
   ],
