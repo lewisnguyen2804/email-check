9a4783bc30b5af8d7a9b46bc9c01257dfecd2ecf
vienpv <vien.pham@terralogic.com>
2025-06-27 17:19:26 +0700

    Fix bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import { TooltipProvider } from "@/components/ui/tooltip";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "HROS Emails",
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <NextTopLoader color="#DC143C" showSpinner={false} />
         <TooltipProvider>{children}</TooltipProvider>
       </body>
